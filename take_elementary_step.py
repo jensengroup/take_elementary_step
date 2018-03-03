e126184247120bf3793ce561e1111b7788aa1c63
Jan H. Jensen <jhjensen@chem.ku.dk>
2018-03-03 15:24:46 +0100

    remove isomeric=True

--- take_elementary_step.py
+++ take_elementary_step.py
@@ -8,245 +8,13 @@ from collections import defaultdict
 import copy
 import time
 import numpy as np
+import xyz2mol
 from io import StringIO
 import sys
 #sio = sys.stderr = StringIO()
-from rdkit import rdBase
-print(rdBase.rdkitVersion)
+#from rdkit import rdBase
+#print(rdBase.rdkitVersion)
 
-
-
-def getUA(maxValence_list, valence_list):
-    UA = []
-    DU = []
-    for i, (maxValence,valence) in enumerate(zip(maxValence_list, valence_list)):
-        if maxValence - valence > 0:
-            UA.append(i)
-            DU.append(maxValence - valence)
-    return UA,DU
-
-
-def get_BO(AC,valences):
-    BO = AC.copy()
-    BO_valence = list(BO.sum(axis=1))
-    UA,DU = getUA(valences, BO_valence)
-
-    while len(DU) > 1:
-        UA_pairs = list(itertools.combinations(UA, 2))
-
-        for i,j in UA_pairs:
-            if BO[i,j] > 0:
-                BO[i,j] += 1
-                BO[j,i] += 1
-                break
-        
-        BO_valence = list(BO.sum(axis=1))
-        UA_new, DU_new = getUA(valences, BO_valence)
-
-        if DU_new != DU:
-            UA = copy.copy(UA_new)
-            DU = copy.copy(DU_new)
-        else:
-            break
-    
-    return BO
-
-
-def BO_is_OK(BO,AC,charge,DU,atomic_valence_electrons,atomicNumList):
-
-    q = 0
-    if heterolytic:
-        BO_valences = list(BO.sum(axis=1))
-        for i,atom in enumerate(atomicNumList):
-            q += get_atomic_charge(atom,atomic_valence_electrons[atom],BO_valences[i])
-            if atom == 6:
-                number_of_single_bonds_to_C = list(BO[i,:]).count(1)
-                if number_of_single_bonds_to_C == 2 and BO_valences[i] == 2:
-                    q += 1
-                if number_of_single_bonds_to_C == 3 and q + 1 < charge:
-                    q += 2
-    
-    if (BO-AC).sum() == sum(DU) and charge == q:
-        return True
-    else:
-        return False
-
-
-def get_atomic_charge(atom,atomic_valence_electrons,BO_valence):
-    if atom == 1:
-        charge = 1 - BO_valence
-    elif atom == 5:
-        charge = 3 - BO_valence
-    elif atom == 15 and BO_valence == 5:
-        charge = 0
-    elif atom == 16 and BO_valence == 6:
-        charge = 0
-    else:
-        charge = atomic_valence_electrons - 8 + BO_valence
-          
-    return charge
-
-
-def clean_charges(mol):
-# this is a temporary hack. The real solution is to generate several BO matrices in AC2BO and pick the one
-# with the lowest number of atomic charges
-#
-    rxn_smarts = ['[N+:1]=[*:2]-[O-:3]>>[N+0:1]-[*:2]=[O-0:3]',
-                  '[N+:1]=[*:2]-[*:3]=[*:4]-[O-:5]>>[N+0:1]-[*:2]=[*:3]-[*:4]=[O-0:5]']
-
-    for smarts in rxn_smarts:
-        patt = Chem.MolFromSmarts(smarts.split(">>")[0])
-        while mol.HasSubstructMatch(patt):
-            rxn = AllChem.ReactionFromSmarts(smarts)
-            ps = rxn.RunReactants((mol,))
-            mol = ps[0][0]
-                    
-    return mol
-    
-
-
-def BO2mol(mol,BO_matrix, atomicNumList,atomic_valence_electrons,mol_charge):
-# based on code written by Paolo Toscani
-
-    l = len(BO_matrix)
-    l2 = len(atomicNumList)
-    BO_valences = list(BO_matrix.sum(axis=1))
-    
-    if (l != l2):
-        raise RuntimeError('sizes of adjMat ({0:d}) and atomicNumList '
-            '{1:d} differ'.format(l, l2))
-
-    rwMol = Chem.RWMol(mol)
-
-    bondTypeDict = {
-        1: Chem.BondType.SINGLE,
-        2: Chem.BondType.DOUBLE,
-        3: Chem.BondType.TRIPLE
-    }
-    
-    for i in range(l):
-        for j in range(i + 1, l):
-            bo = int(round(BO_matrix[i, j]))
-            if (bo == 0):
-                continue
-            bt = bondTypeDict.get(bo, Chem.BondType.SINGLE)
-            rwMol.AddBond(i, j, bt)
-    mol = rwMol.GetMol()
-    
-    if heterolytic:
-        mol = set_atomic_charges(mol,atomicNumList,atomic_valence_electrons,BO_valences,BO_matrix,mol_charge)
-    else:
-        mol = set_atomic_radicals(mol,atomicNumList,atomic_valence_electrons,BO_valences)
-  
-    return mol
-
-
-def set_atomic_charges(mol,atomicNumList,atomic_valence_electrons,BO_valences,BO_matrix,mol_charge):
-    q = 0
-    for i,atom in enumerate(atomicNumList):
-        a = mol.GetAtomWithIdx(i)
-        charge = get_atomic_charge(atom,atomic_valence_electrons[atom],BO_valences[i])
-        q += charge
-        if atom == 6:
-            number_of_single_bonds_to_C = list(BO_matrix[i,:]).count(1)
-            if number_of_single_bonds_to_C == 2 and BO_valences[i] == 2:
-                    q += 1
-                    charge = 0        
-            if number_of_single_bonds_to_C == 3 and q + 1 < mol_charge:
-                    q += 2
-                    charge = 1
- 
-        if (abs(charge) > 0):
-            a.SetFormalCharge(charge)
-    #rdmolops.SanitizeMol(mol)
-    
-    mol = clean_charges(mol)
-    
-    return mol
-
-
-def set_atomic_radicals(mol,atomicNumList,atomic_valence_electrons,BO_valences):
-# The number of radical electrons = absolute atomic charge
-    for i,atom in enumerate(atomicNumList):
-        a = mol.GetAtomWithIdx(i)
-        charge = get_atomic_charge(atom,atomic_valence_electrons[atom],BO_valences[i])
-
-        if (abs(charge) > 0):
-            a.SetNumRadicalElectrons(abs(charge))
-            
-    return mol
-
-
-def AC2BO(AC,atomicNumList,charge):
-    atomic_valence = defaultdict(list)
-    atomic_valence[1] = [1]
-    atomic_valence[6] = [4]
-    atomic_valence[7] = [4,3]
-    atomic_valence[8] = [2,1]
-    atomic_valence[9] = [1]
-    atomic_valence[14] = [4]
-    atomic_valence[15] = [5,4,3]
-    atomic_valence[16] = [6,4,2]
-    atomic_valence[17] = [1]
-    atomic_valence[35] = [1]
-    atomic_valence[53] = [1]
-    
-
-    atomic_valence_electrons = {}
-    atomic_valence_electrons[1] = 1
-    atomic_valence_electrons[6] = 4
-    atomic_valence_electrons[7] = 5
-    atomic_valence_electrons[8] = 6
-    atomic_valence_electrons[9] = 7
-    atomic_valence_electrons[14] = 4
-    atomic_valence_electrons[15] = 5
-    atomic_valence_electrons[16] = 6
-    atomic_valence_electrons[17] = 7
-    atomic_valence_electrons[35] = 7
-    atomic_valence_electrons[53] = 7
-
-    valences_list_of_lists = []
-    for atomicNum in atomicNumList:
-        valences_list_of_lists.append(atomic_valence[atomicNum])
-
-    valences_list = list(itertools.product(*valences_list_of_lists))
-
-    best_BO = AC.copy()
-
-    for valences in valences_list:
-        AC_valence = list(AC.sum(axis=1))
-        UA,DU_from_AC = getUA(valences, AC_valence)
-        if len(UA) == 0 or BO_is_OK(AC,AC,charge,DU_from_AC,atomic_valence_electrons,atomicNumList):
-            best_BO = AC.copy()
-            break
-        else:
-            BO = get_BO(AC,valences)
-            if BO_is_OK(BO,AC,charge,DU_from_AC,atomic_valence_electrons,atomicNumList):
-                best_BO = BO.copy()
-                break
-            elif BO.sum() > best_BO.sum():
-                    best_BO = BO.copy()
-
-    return best_BO,atomic_valence_electrons
-
-
-def AC2mol(mol,AC,atomicNumList,charge):
-    BO,atomic_valence_electrons = AC2BO(AC,atomicNumList,charge)
-    mol = BO2mol(mol,BO, atomicNumList,atomic_valence_electrons,charge)
-    
-    return mol
-
-
-def get_proto_mol(atomicNumList):
-    mol = Chem.MolFromSmarts("[#"+str(atomicNumList[0])+"]")
-    rwMol = Chem.RWMol(mol)
-    for i in range(1,len(atomicNumList)):
-        a = Chem.Atom(atomicNumList[i])
-        rwMol.AddAtom(a)
-    
-    mol = rwMol.GetMol()
-
-    return mol
 
 def get_C(R,num_atoms):
     C_matrices = []
@@ -438,9 +206,9 @@ def I_is_valid(I,atomicNumList):
     return True
 
 
-def take_elementary_step(mol,E_cutoff):
+def take_elementary_step(mol,charge,E_cutoff,heterolytic):
     atomicNumList = [a.GetAtomicNum() for a in mol.GetAtoms()]
-    proto_mol = get_proto_mol(atomicNumList)
+    proto_mol = xyz2mol.get_proto_mol(atomicNumList)
 
     AC = Chem.GetAdjacencyMatrix(mol)
     
@@ -453,7 +221,7 @@ def take_elementary_step(mol,E_cutoff):
     raw_molecules = []
     for I in I_elementary:
         if I_is_valid(I,atomicNumList):
-            newmol = AC2mol(proto_mol,I,atomicNumList,charge)
+            newmol = xyz2mol.AC2mol(proto_mol,I,atomicNumList,charge,heterolytic)
             raw_smiles = Chem.MolToSmiles(newmol)
             if raw_smiles not in raw_smiles_list:
                 raw_smiles_list.append(raw_smiles)
@@ -490,11 +258,7 @@ if __name__ == "__main__":
         rdmolops.Kekulize(mol, clearAromaticFlags = True)
         charge = Chem.GetFormalCharge(mol)
         mol = Chem.AddHs(mol)
-        elementary_smiles, elementary_mols = take_elementary_step(mol,E_cutoff)
+        elementary_smiles, elementary_mols = take_elementary_step(mol,charge,E_cutoff,heterolytic)
 
         print "len(elementary_smiles)",len(elementary_smiles)
         print elementary_smiles
-
-
-
-
